2d5481fbdc07636992867ed7c8ce39b58298199a
WilliamKruse <williamkruse4@gmail.com>
2022-06-01 13:44:14 +0200

    state

---
+++ festivalprojekt/Shared/Models/AppStatus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace festivalprojekt.Shared.Models
+    
+{
+	public class AppStatus
+	{
+
+        public bool LoggedIn { get; private set; }
+
+        public event Action OnChange;
+
+        public void Log(bool nool)
+        {
+            LoggedIn = nool;
+          
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+      
+	}
+}
+
